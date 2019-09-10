7d4f9f9ac783c127a30399f67d513ab1c7bb6422
DuoLaGMeng <1009156204@qq.com>
2019-09-10 21:13:07 +0800

    add check cookies

--- src/main/java/life/guangsi/community/mapper/UserMapper.java
+++ src/main/java/life/guangsi/community/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package life.guangsi.community.mapper;
 import life.guangsi.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -10,4 +11,6 @@ public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token=#{token}")
+    User findByToken(String token);
 }
