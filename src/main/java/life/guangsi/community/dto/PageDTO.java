ccf6f2cfb6e7868a37f5c96f8cd17ee1f7bed68d
DuoLaGMeng <1009156204@qq.com>
2019-09-15 09:54:37 +0800

    add profile page

---
+++ src/main/java/life/guangsi/community/dto/PageDTO.java
@@ -0,0 +1,61 @@
+package life.guangsi.community.dto;
+
+import lombok.Data;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PageDTO {
+    private List<QuestionDTO> questionDTOS;
+    private boolean hasPrevious;
+    private boolean hasNext;
+    private boolean showFisrtPage;
+    private boolean showEndPage;
+    private Integer currentPage;//page
+    private Integer totalPage;
+    private List<Integer> pages = new ArrayList<>();
+
+    public void setPagination(Integer totalPage, Integer page) {
+        this.totalPage = totalPage;
+        this.currentPage = page;
+        pages.add(page);
+        for (int i = 1; i <= 3; i++) {
+            if (page - i > 0) {
+                pages.add(0, page - i);
+            }
+            if (page + i <= totalPage) {
+                pages.add(page + i);
+            }
+        }
+
+        //上一页判断
+        if (page == 1) {
+            hasPrevious = false;
+        } else {
+            hasPrevious = true;
+        }
+
+        //下一页判断
+        if (page == totalPage) {
+            hasNext = false;
+        } else {
+            hasNext = true;
+        }
+
+        //第一页判断
+        if (pages.contains(1)) {
+            showFisrtPage = false;
+        } else {
+            showFisrtPage = true;
+        }
+
+        //最后一页判断
+        if (pages.contains(totalPage)) {
+            showEndPage = false;
+        } else {
+            showEndPage = true;
+        }
+    }
+}
--- src/main/java/life/guangsi/community/service/QuestionService.java
+++ src/main/java/life/guangsi/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.guangsi.community.service;
 
+import life.guangsi.community.dto.PageDTO;
 import life.guangsi.community.dto.QuestionDTO;
 import life.guangsi.community.mapper.QuestionMapper;
 import life.guangsi.community.mapper.UserMapper;
@@ -8,6 +9,7 @@ import life.guangsi.community.model.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,16 +23,74 @@ public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PageDTO list(Integer page, Integer size) {
+        Integer totalPage;
+        PageDTO pageDTO = new PageDTO();
+        Integer totalcount = questionMapper.count();
+
+        if (totalcount % size == 0) {
+            totalPage = totalcount / size;
+        } else {
+            totalPage = (totalcount / size) + 1;
+        }
+
+        if(page < 1){
+            page = 1;
+        }
+        if(page > totalPage){
+            page = totalPage;
+        }
+
+        pageDTO.setPagination(totalPage, page);
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOS = new ArrayList<>();
-        for (Question question: questions) {
+
+        for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
-            BeanUtils.copyProperties(question,questionDTO);
+            BeanUtils.copyProperties(question, questionDTO);
             questionDTO.setUser(user);
             questionDTOS.add(questionDTO);
         }
-        return questionDTOS;
+        pageDTO.setQuestionDTOS(questionDTOS);
+
+        return pageDTO;
+    }
+
+    public PageDTO list(Integer userId, Integer page, Integer size) {
+        Integer totalPage;
+        PageDTO pageDTO = new PageDTO();
+        Integer totalcount = questionMapper.countByUserId(userId);
+
+        if (totalcount % size == 0) {
+            totalPage = totalcount / size;
+        } else {
+            totalPage = (totalcount / size) + 1;
+        }
+
+        if(page < 1){
+            page = 1;
+        }
+        if(page > totalPage){
+            page = totalPage;
+        }
+
+        pageDTO.setPagination(totalPage, page);
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.listByUserId(userId,offset, size);
+        List<QuestionDTO> questionDTOS = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOS.add(questionDTO);
+        }
+        pageDTO.setQuestionDTOS(questionDTOS);
+
+        return pageDTO;
     }
 }
