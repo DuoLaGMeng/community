10cf72c262d826333870f12f6e1da23c2291c925
DuoLaGMeng <1009156204@qq.com>
2019-09-15 15:58:24 +0800

    add question desc

--- src/main/java/life/guangsi/community/service/QuestionService.java
+++ src/main/java/life/guangsi/community/service/QuestionService.java
@@ -93,4 +93,13 @@ public class QuestionService {
 
         return pageDTO;
     }
+
+    public QuestionDTO getById(Integer id) {
+        QuestionDTO questionDTO = new QuestionDTO();
+        Question question = questionMapper.getById(id);
+        User user = userMapper.findById(question.getCreator());
+        BeanUtils.copyProperties(question, questionDTO);
+        questionDTO.setUser(user);
+        return questionDTO;
+    }
 }
